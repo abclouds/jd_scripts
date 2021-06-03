bc1b15faa4e8d0d043978079bbaff1d8a78fa26f
akuticlouds <akuticlouds@gmail.com>
2021-06-03 21:46:10 +0800

    update

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -11,10 +11,8 @@ if [ -f "/scripts/logs/pull.lock" ]; then
 else
   echo "设定远程仓库地址..."
   cd /scripts
-  git remote set-url origin ""
-  git reset --hard
   echo "git pull拉取最新代码..."
-  git -C /scripts pull --rebase
+  git pull
   echo "npm install 安装最新依赖"
   npm install --prefix /scripts
 fi
